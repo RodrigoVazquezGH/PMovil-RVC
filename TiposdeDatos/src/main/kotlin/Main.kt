005b3597c8175b58032d632a52575a0de4b5f1fb
RodrigoVazquezGH <145868874+RodrigoVazquezGH@users.noreply.github.com>
2023-10-03 20:09:18 -0600

    Add files via upload

---
+++ TiposdeDatos/src/main/kotlin/Main.kt
@@ -0,0 +1,12 @@
+const val Pi = 3.1416
+
+fun main(){
+
+    val numDecimal: Double = 5.5
+
+    val radioCuadrado = numDecimal*numDecimal
+    val areaCirculo = radioCuadrado*Pi
+
+    println("El area del circulo es $areaCirculo")
+
+}
